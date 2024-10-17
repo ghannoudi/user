a1224760a2988a7db444218895ccac0a0dedeaa6
ghannoud chawki <ghannoudichawki63.com>
2024-10-17 22:20:02 +0200

    test

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,15 +1,18 @@
 <template>
+  <div>
   <HelloWorld />
-
+  <HealthCheck />
+</div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
-
+import HealthCheck from "./components/Health.vue"; 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    HealthCheck
   }
 }
 </script>
