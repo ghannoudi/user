cf6b0575e30cbe4aaac0109d377bc183a5ad2d7e
ghannoud chawki <ghannoudichawki63.com>
2024-10-20 01:56:11 +0200

    adding branch test

--- back/obj/Debug/net8.0/back.AssemblyInfo.cs
+++ back/obj/Debug/net8.0/back.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2418204fdf0519ae2162862f6341ff9fe0a9513")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f419efcebee5f0f92adc46a2d8540afe0a8eadaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
